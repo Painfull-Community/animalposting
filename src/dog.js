bd170cb4b537a6e60bc5932f6068c8dafd4d2107
Vukky <46850780+Vukkyy@users.noreply.github.com>
2022-05-13 19:22:04 +0000

    fix dog?

--- src/dog.js
+++ src/dog.js
@@ -1,5 +1,6 @@
 module.exports = {
     execute: function (message, args) {
+        const axios = require("axios");
         axios.get("https://dog.ceo/api/breeds/image/random")
             .then(json => {
                 message.reply(json.data.message)
