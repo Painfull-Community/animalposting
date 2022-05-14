14f6e8a002e8ccf65239ce37d865e0b5c2e476c5
Vukky <46850780+Vukkyy@users.noreply.github.com>
2022-05-14 12:13:17 +0000

    yeah

--- src/cat.js
+++ src/cat.js
@@ -1,5 +1,9 @@
 module.exports = {
     execute: function (message, args) {
+        if(args[0] == "simulation") {
+            // easter egg :)
+            return message.reply("https://i.imgur.com/0kfq8kA.gif")
+        }
         message.reply("https://cataas.com/cat?" + new Date().getTime())
     }
 }
